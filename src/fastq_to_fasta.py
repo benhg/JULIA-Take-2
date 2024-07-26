0ee6e6e7943b8e5230edc7a88ba8384a5624da62
Ben Glick <b.glick@sapeon.com>
2024-07-26 16:02:39 -0700

    add fastq to fasta

---
+++ src/fastq_to_fasta.py
@@ -0,0 +1,13 @@
+"""
+The raw reads are in fastq format, but it'll be easier to deal with them in fasta format 
+"""
+from Bio import SeqIO
+from glob import glob
+
+source_dir = "/home/labs/binford/raw_reads"
+out_dir = "/home/labs/binford/raw_reads_fasta"
+
+
+for file in glob(f"{source_dir}/*.fastq"):
+    new_filename = file.replace("fastq", "fasta").replace(source_dir, out_dir)
+    print(file, new_filename)
