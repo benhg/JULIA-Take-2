28b4f4e6c012b33858f436a8d8a3a5094eb1df6d
Ben Glick <b.glick@sapeon.com>
2024-07-26 15:35:02 -0700

    spaces

--- src/tag_transcriptomes.py
+++ src/tag_transcriptomes.py
@@ -15,13 +15,15 @@ output_file = f"{source_dir}/all_assembled_transcriptomes.fasta"
 global_file = {}
 
 for file in glob(f"{source_dir}/*.fasta"):
-	sample_id = file.split("/")[-1].split(".fasta")[0]
-	s_num = sample_id.split("s")[-1].split("_")[0]
-	lane = 1 if int(s_num) < 12 else 2
-	print(sample_id, lane)
-	records = list(SeqIO.parse(file, "fasta"))
-	for record in records:
-		record.id = f"{record.id} sample={sample_id} lane={lane}"
+    sample_id = file.split("/")[-1].split(".fasta")[0]
+    s_num = sample_id.split("s")[-1].split("_")[0]
+    lane = 1 if int(s_num) < 12 else 2
+    print(sample_id, lane)
+    records = list(SeqIO.parse(file, "fasta"))
+    for record in records:
+        record.id = f"{record.id} sample={sample_id} lane={lane}"
+
+    print(records[0].id)
 
 
 
