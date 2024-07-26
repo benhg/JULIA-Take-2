91fdc5e18d177ef2a650512a9ac547c1d82c1ab4
Ben Glick <b.glick@sapeon.com>
2024-07-26 16:05:52 -0700

    add fastq to fasta

--- src/fastq_to_fasta.py
+++ src/fastq_to_fasta.py
@@ -10,4 +10,7 @@ out_dir = "/home/labs/binford/raw_reads_fasta"
 
 for file in glob(f"{source_dir}/*.fastq"):
     new_filename = file.replace("fastq", "fasta").replace(source_dir, out_dir)
-    print(file, new_filename)
+    with open(file, "r") as old_handle, open(new_filename, "w") as new_handle:
+        sequences = SeqIO.parse(old_handle, "fastq")
+        count = SeqIO.write(sequences, new_handle, "fasta")
+        print(f"extracted {count} sequences from {old_handle.split("/"[-1])}")
