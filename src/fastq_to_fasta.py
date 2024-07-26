f496a3f14f01141d605b793c83f49221609f6b8f
Ben Glick <b.glick@sapeon.com>
2024-07-26 16:08:25 -0700

    multiprocess

--- src/fastq_to_fasta.py
+++ src/fastq_to_fasta.py
@@ -3,14 +3,19 @@ The raw reads are in fastq format, but it'll be easier to deal with them in fast
 """
 from Bio import SeqIO
 from glob import glob
+import multiprocessing
 
 source_dir = "/home/labs/binford/raw_reads"
 out_dir = "/home/labs/binford/raw_reads_fasta"
 
 
-for file in glob(f"{source_dir}/*.fastq"):
+def process_file(file)
     new_filename = file.replace("fastq", "fasta").replace(source_dir, out_dir)
     with open(file, "r") as old_handle, open(new_filename, "w") as new_handle:
         sequences = SeqIO.parse(old_handle, "fastq")
         count = SeqIO.write(sequences, new_handle, "fasta")
         print(f"extracted {count} sequences from {old_handle.split('/'[-1])}")
+
+pool = multiprocessing.Pool()
+
+work = pool.map(process_file, [f for file in glob(f"{source_dir}/*.fastq")])
