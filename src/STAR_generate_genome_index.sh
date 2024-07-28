0e4452bdc3fe093388e4a35ee829d92366ad35a8
Ben Glick <b.glick@sapeon.com>
2024-07-27 19:34:33 -0700

    fix

--- src/STAR_generate_genome_index.sh
+++ src/STAR_generate_genome_index.sh
@@ -3,5 +3,5 @@
 
 /home/glick/STAR/bin/Linux_x86_64/STAR --runThreadN 48 --runMode genomeGenerate \
 --genomeDir /home/labs/binford/all_transcriptomes_genome_dir \
---genomeFastaFiles /home/labs/binford/Assembled_Untranslated_Transcriptomes/all_assembled_transcriptomes.fasta
+--genomeFastaFiles /home/labs/binford/Assembled_Untranslated_Transcriptomes/all_assembled_transcriptomes.fasta \
 --limitGenomeGenerateRAM=322122547200
