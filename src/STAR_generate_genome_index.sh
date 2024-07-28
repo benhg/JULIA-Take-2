fbb282f915b50ddedb23fb47014c5d7631fa7305
Ben Glick <b.glick@sapeon.com>
2024-07-27 17:39:41 -0700

    fix

--- src/STAR_generate_genome_index.sh
+++ src/STAR_generate_genome_index.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=48
 
-path/to/STAR --runThreadN 48 --runMode genomeGenerate --genomeDir /home/labs/binford/all_transcriptomes_genome_dir
+/home/glick/STAR/bin/Linux_x86_64/STAR --runThreadN 48 --runMode genomeGenerate\
+--genomeDir /home/labs/binford/all_transcriptomes_genome_dir\
 --genomeFastaFiles /home/labs/binford/Assembled_Untranslated_Transcriptomes/all_assembled_transcriptomes.fasta
