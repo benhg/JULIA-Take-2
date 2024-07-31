93502f00c6efcfcab6b5edaf5a671e3defb947d0
Ben Glick <b.glick@sapeon.com>
2024-07-30 17:03:36 -0700

    fix path

--- src/bowtie_generate_index_all.sh
+++ src/bowtie_generate_index_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=48
 
-bowtie2-build --large-index Assembled_Untranslated_Transcriptomes/all_assembled_transcriptomes.fasta all_untranslated_transcriptomes_index
+bowtie2-build --large-index /home/labs/binford/Assembled_Untranslated_Transcriptomes/all_assembled_transcriptomes.fasta /home/labs/binford/all_untranslated_transcriptomes_index
