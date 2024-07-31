2b4be960b717e11e264fbc3eb04d2902cc6c2740
Ben Glick <b.glick@sapeon.com>
2024-07-30 17:08:58 -0700

    typo

--- src/aggregate_tag_transcriptomes_lanes.py
+++ src/aggregate_tag_transcriptomes_lanes.py
@@ -32,9 +32,9 @@ for file in glob(f"{source_dir}/*.fasta"):
             print("Unknown lane.......")
 
 # Output to new files
-with open(output_file = f"{source_dir}/all_assembled_transcriptomes_lane_1.fasta", "w") as output_handle:
+with open(f"{source_dir}/all_assembled_transcriptomes_lane_1.fasta", "w") as output_handle:
     SeqIO.write(global_file_lane_1, output_handle, "fasta")
 
-with open(output_file = f"{source_dir}/all_assembled_transcriptomes_lane_2.fasta", "w") as output_handle:
+with open(f"{source_dir}/all_assembled_transcriptomes_lane_2.fasta", "w") as output_handle:
     SeqIO.write(global_file_lane_2, output_handle, "fasta")
 
