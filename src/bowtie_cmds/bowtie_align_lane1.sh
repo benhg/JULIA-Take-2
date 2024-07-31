bd7b132687ecb53ce193cf28d2b2bd7a9b37302f
Ben Glick <b.glick@sapeon.com>
2024-07-31 12:53:36 -0700

    add bowtie align command

--- src/bowtie_cmds/bowtie_align_lane1.sh
+++ src/bowtie_cmds/bowtie_align_lane1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=48
 
-bowtie2 -f --threads 48 -x /home/labs/binford/all_untranslated_transcriptomes_index_lane_1/all_untranslated_transcriptomes_index_lane_1 -1 /home/labs/binford//home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane1_R1.fasta -2 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane1_R2.fasta
+bowtie2 -f --threads 48 -x /home/labs/binford/all_untranslated_transcriptomes_index_lane_1/all_untranslated_transcriptomes_index_lane_1 -1 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane1_R1.fasta -2 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane1_R2.fasta
