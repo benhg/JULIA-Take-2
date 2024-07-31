090a4ee3781bde938172153edb118cd47347f717
Ben Glick <b.glick@sapeon.com>
2024-07-30 17:19:31 -0700

    48 core

--- src/bowtie_cmds/bowtie_generate_index_lane_2.sh
+++ src/bowtie_cmds/bowtie_generate_index_lane_2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=48
 
-bowtie2-build --large-index /home/labs/binford/Assembled_Untranslated_Transcriptomes/all_assembled_transcriptomes_lane_2.fasta /home/labs/binford/all_untranslated_transcriptomes_index_lane_2
+bowtie2-build -p 48 --large-index /home/labs/binford/Assembled_Untranslated_Transcriptomes/all_assembled_transcriptomes_lane_2.fasta /home/labs/binford/all_untranslated_transcriptomes_index_lane_2
