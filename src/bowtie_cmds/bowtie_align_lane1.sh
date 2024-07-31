5f9962c43927494e282b01ad4f5bdbe0312614b7
Ben Glick <b.glick@sapeon.com>
2024-07-31 15:29:20 -0700

    add saving output

--- src/bowtie_cmds/bowtie_align_lane1.sh
+++ src/bowtie_cmds/bowtie_align_lane1.sh
@@ -3,4 +3,4 @@
 
 # NOTE: Job 1729 on Bacon is lane1
 
-bowtie2 -f --threads 48 -x /home/labs/binford/all_untranslated_transcriptomes_index_lane_1/all_untranslated_transcriptomes_index_lane_1 -1 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane1_R1.fasta -2 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane1_R2.fasta
+bowtie2 --time --un /home/labs/binford/lane1_collateral/unpaired_unaligned.txt --al /home/labs/binford/lane1_collateral/unpaired_aligned.txt --un-conc /home/labs/binford/lane1_collateral/paired_conc_unaligned.txt --al-conc /home/labs/binford/lane1_collateral/paired_conc_aligned.txt  --met-file /home/labs/binford/lane1_collateral/lane1_bt_metrics.txt -f --threads 48 -x /home/labs/binford/all_untranslated_transcriptomes_index_lane_1/all_untranslated_transcriptomes_index_lane_1 -1 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane1_R1.fasta -2 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane1_R2.fasta
--- src/bowtie_cmds/bowtie_align_lane2.sh
+++ src/bowtie_cmds/bowtie_align_lane2.sh
@@ -3,4 +3,4 @@
 
 # NOTE: Job 1730 on Lettuce is lane2
 
-bowtie2 -f --threads 48 -x /home/labs/binford/all_untranslated_transcriptomes_index_lane_2/all_untranslated_transcriptomes_index_lane_2 -1 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane2_R1.fasta -2 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane2_R2.fasta
+bowtie2 --time --un /home/labs/binford/lane2_collateral/unpaired_unaligned.txt --al /home/labs/binford/lane2_collateral/unpaired_aligned.txt --un-conc /home/labs/binford/lane2_collateral/paired_conc_unaligned.txt --al-conc /home/labs/binford/lane2_collateral/paired_conc_aligned.txt  --met-file /home/labs/binford/lane2_collateral/lane1_bt_metrics.txt -f --threads 48 -x /home/labs/binford/all_untranslated_transcriptomes_index_lane_2/all_untranslated_transcriptomes_index_lane_2 -1 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane2_R1.fasta -2 /home/labs/binford/raw_reads_fasta_tagged_batched/combined_files/lane2_R2.fasta
