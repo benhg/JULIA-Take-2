592f1e6085086b049d56763b5c4f4527cf599319
Ben Glick <glick@glick.cloud>
2024-08-07 15:43:50 -0700

    glob

--- src/bowtie_alignment_runs.py
+++ src/bowtie_alignment_runs.py
@@ -33,14 +33,14 @@ for lane in range(1,3):
             print(reads_sample_id, index_id)
 
 
-            print(f"{combined_files_dir}/lane{lane}-s{index_id}*R1*")
+            #print(f"{combined_files_dir}/lane{lane}-s{index_id}*R1*")
             dir_1_filename = glob(f"{combined_files_dir}/lane{lane}-s{reads_sample_id}*R1*")
-            print(dir_1_filename)
+            #print(dir_1_filename)
             dir_1_filename = glob(f"{combined_files_dir}/lane{lane}-s{reads_sample_id}*R1*")[0]
             dir_2_filename = dir_1_filename.replace("R1", "R2")
 
             sbatch_text = sbatch_template.format(index_id, index_id, dir_1_filename, dir_2_filename, index_id, index_id, reads_sample_id)
-            print(dir_1_filename, dir_2_filename, index_id)
+            #print(dir_1_filename, dir_2_filename, index_id)
             # with open(f"bowtie_cmds/gen_index_{reads_sample_id}_s{reads_sample_id}.sh", "w") as fh:
             #     fh.write(sbatch_text)
             # print(subprocess.check_output(f"sbatch bowtie_cmds/gen_index_{reads_sample_id}_s{reads_sample_id}.sh", shell=True))
