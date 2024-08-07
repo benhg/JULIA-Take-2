6ce530283a5176ac6bd55f07497dc3cb2e556e02
Ben Glick <glick@glick.cloud>
2024-08-07 15:53:01 -0700

    testing

--- src/bowtie_alignment_runs.py
+++ src/bowtie_alignment_runs.py
@@ -42,7 +42,7 @@ for lane in range(1,3):
 
             sbatch_text = sbatch_template.format(index_id, index_id, dir_1_filename, dir_2_filename, index_id, index_id, reads_sample_id)
             #print(dir_1_filename, dir_2_filename, index_id)
-            with open(f"bowtie_cmds/gen_index_{reads_sample_id}_s{reads_sample_id}.sh", "w") as fh:
+            with open(f"bowtie_cmds/gen_index_{index_id}_s{reads_sample_id}.sh", "w") as fh:
                 fh.write(sbatch_text)
             #print(subprocess.check_output(f"sbatch bowtie_cmds/gen_index_{reads_sample_id}_s{reads_sample_id}.sh", shell=True))
 
@@ -55,7 +55,7 @@ dir_2_filename = dir_1_filename.replace("R1", "R2")
 
 sbatch_text = sbatch_template.format("012", "012", dir_1_filename, dir_2_filename, "012", "012", "001")
 with open(f"bowtie_cmds/gen_index_012_001s.sh", "w") as fh:
-                fh.write(sbatch_text)
+    fh.write(sbatch_text)
     #print(subprocess.check_output(f"sbatch bowtie_cmds/gen_index_012_001s.sh", shell=True))
 
 
@@ -65,5 +65,5 @@ dir_2_filename = dir_1_filename.replace("R1", "R2")
 
 sbatch_text = sbatch_template.format("005", "005", dir_1_filename, dir_2_filename, "005", "005", "021")
 with open(f"bowtie_cmds/gen_index_012_001s.sh", "w") as fh:
-                fh.write(sbatch_text)
+    fh.write(sbatch_text)
     #print(subprocess.check_output(f"sbatch bowtie_cmds/gen_index_012_001s.sh", shell=True))
