fc297a9d9f0fa6d090c7e3c4c965b35e3454cef8
Ben Glick <glick@glick.cloud>
2024-08-07 15:49:49 -0700

    spot check

--- src/bowtie_alignment_runs.py
+++ src/bowtie_alignment_runs.py
@@ -36,16 +36,34 @@ for lane in range(1,3):
 
 
             #print(f"{combined_files_dir}/lane{lane}-s{index_id}*R1*")
-            dir_1_filename = glob(f"{combined_files_dir}/lane{lane}-s{reads_sample_id}*R1*")
-            #print(dir_1_filename)
             dir_1_filename = glob(f"{combined_files_dir}/lane{lane}-s{reads_sample_id}*R1*")[0]
+            #print(dir_1_filename)
             dir_2_filename = dir_1_filename.replace("R1", "R2")
 
             sbatch_text = sbatch_template.format(index_id, index_id, dir_1_filename, dir_2_filename, index_id, index_id, reads_sample_id)
             #print(dir_1_filename, dir_2_filename, index_id)
-            # with open(f"bowtie_cmds/gen_index_{reads_sample_id}_s{reads_sample_id}.sh", "w") as fh:
-            #     fh.write(sbatch_text)
-            # print(subprocess.check_output(f"sbatch bowtie_cmds/gen_index_{reads_sample_id}_s{reads_sample_id}.sh", shell=True))
+            with open(f"bowtie_cmds/gen_index_{reads_sample_id}_s{reads_sample_id}.sh", "w") as fh:
+                fh.write(sbatch_text)
+            print(subprocess.check_output(f"sbatch bowtie_cmds/gen_index_{reads_sample_id}_s{reads_sample_id}.sh", shell=True))
 
 ## Special Cases
-print(q)
+## Please run s001 raw reads against s012 transcripts, and s021 raw reads against s005
+
+## S001 against s012
+dir_1_filename = glob(f"{combined_files_dir}/lane1-s001*R1*")[0]
+dir_2_filename = dir_1_filename.replace("R1", "R2")
+
+sbatch_text = sbatch_template.format("012", "012", dir_1_filename, dir_2_filename, "012", "012", "001")
+with open(f"bowtie_cmds/gen_index_012_001s.sh", "w") as fh:
+                fh.write(sbatch_text)
+    print(subprocess.check_output(f"sbatch bowtie_cmds/gen_index_012_001s.sh", shell=True))
+
+
+## S021 against s005 
+dir_1_filename = glob(f"{combined_files_dir}/lane1-s021*R1*")[0]
+dir_2_filename = dir_1_filename.replace("R1", "R2")
+
+sbatch_text = sbatch_template.format("005", "005", dir_1_filename, dir_2_filename, "005", "005", "021")
+with open(f"bowtie_cmds/gen_index_012_001s.sh", "w") as fh:
+                fh.write(sbatch_text)
+    print(subprocess.check_output(f"sbatch bowtie_cmds/gen_index_012_001s.sh", shell=True))
