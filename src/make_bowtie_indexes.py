27ec79b530d72e562ab9d80e25908500924555ac
Ben Glick <glick@glick.cloud>
2024-08-07 10:17:02 -0700

    make the indexes

---
+++ src/make_bowtie_indexes.py
@@ -0,0 +1,23 @@
+"""
+Make separate bowtie indexes for each sample
+
+They'll go into /home/labs/binford/single_sample_indexes/s_{XYZ}/
+"""
+
+import subprocess
+
+sbatch_template = """
+#!/bin/bash
+#SBATCH --cpus-per-task=48
+
+mkdir -p /home/labs/binford/single_sample_indexes/s{}
+
+bowtie2-build --threads 48 --large-index /home/labs/binford/Assembled_Untranslated_Transcriptomes/s{}_Trinity.fasta /home/labs/binford/single_sample_indexes/s{}_index/s{}_index
+"""
+
+for i in range(1,23):
+    sample_id = str(i).zfill(3)
+    sbatch_text = sbatch_template.format(sample_id, sample_id, sample_id, sample_id)
+    with open(f"bowtie_cmds/gen_index_s{sample_id}.sh", "w") as fh:
+        fh.write(sbatch_text)
+    subprocess.check_result("sbatch bowtie_cmds/gen_index_s{sample_id}.sh")
