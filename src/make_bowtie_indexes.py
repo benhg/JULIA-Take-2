8f60bb9f2dd4746a133c2aacaa58eb135339c65a
Ben Glick <glick@glick.cloud>
2024-08-07 10:21:01 -0700

    chmod

--- src/make_bowtie_indexes.py
+++ src/make_bowtie_indexes.py
@@ -10,6 +10,7 @@ sbatch_template = """#!/bin/bash
 #SBATCH --cpus-per-task=48
 
 mkdir -p /home/labs/binford/single_sample_indexes/s{}
+chmod 777 /home/labs/binford/single_sample_indexes/s{}
 
 bowtie2-build --threads 48 --large-index /home/labs/binford/Assembled_Untranslated_Transcriptomes/s{}_Trinity.fasta /home/labs/binford/single_sample_indexes/s{}_index/s{}_index
 """
