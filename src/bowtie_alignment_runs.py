0f08450e49414cc491f721f4f2ab927b0e064ac4
Ben Glick <glick@glick.cloud>
2024-08-07 15:32:57 -0700

    typo

--- src/bowtie_alignment_runs.py
+++ src/bowtie_alignment_runs.py
@@ -22,15 +22,17 @@ for lane in range(1,3):
     for i in range(1,12):
         # For each set of raw reads
         for j in range(1, 12):
-        index_id = str(i*lane).zfill(3)
-        reads_sample_id = str(j*lane).zfill(3)
+            index_id = str(i*lane).zfill(3)
+            reads_sample_id = str(j*lane).zfill(3)
 
-        dir_1_filename = glob.glob(f"{combined_files_dir}/lane{lane}-s{index_id}*R1*")
-        print(dir_1_filename) 
-        dir_2_filename = dir_1_filename.replace("R1", "R2")
+            dir_1_filename = glob.glob(f"{combined_files_dir}/lane{lane}-s{index_id}*R1*")
+            print(dir_1_filename) 
+            dir_2_filename = dir_1_filename.replace("R1", "R2")
 
-        sbatch_text = sbatch_template.format(index_id, index_id, dir_1_filename, dir_2_filename, index_id, index_id sample_id)
-        print(dir_1_filename, dir_2_filename, index_id)
-        # with open(f"bowtie_cmds/gen_index_s{sample_id}.sh", "w") as fh:
-        #     fh.write(sbatch_text)
-        # print(subprocess.check_output(f"sbatch /home/glick/JULIA-Take-2/src/bowtie_cmds/gen_index_s{sample_id}.sh", shell=True))
+            sbatch_text = sbatch_template.format(index_id, index_id, dir_1_filename, dir_2_filename, index_id, index_id sample_id)
+            print(dir_1_filename, dir_2_filename, index_id)
+            # with open(f"bowtie_cmds/gen_index_s{sample_id}.sh", "w") as fh:
+            #     fh.write(sbatch_text)
+            # print(subprocess.check_output(f"sbatch /home/glick/JULIA-Take-2/src/bowtie_cmds/gen_index_s{sample_id}.sh", shell=True))
+
+## Special Cases
