f1db1fe3bf26a51624f679608cd2dad4cae2f13b
Ben Glick <glick@glick.cloud>
2024-08-25 15:23:07 -0700

    slurm job time

--- src/generate_summary.py
+++ src/generate_summary.py
@@ -5,8 +5,10 @@ Summary contains the information that Bowtie produced as well as where to get th
 """
 from glob import glob
 import csv
+import subprocess
 
-headers = ["reads_sample", "index_sample", "num_reads", "num_aligned_none", "num_aligned_once", "num_aligned_multiple", "none_alignment_rate", "single_alignment_rate", "multiple_alignment_rate", "num_aligned_any" ,"alignment_rate"]
+
+headers = ["reads_sample", "index_sample", "num_reads", "num_aligned_none", "num_aligned_once", "num_aligned_multiple", "none_alignment_rate", "single_alignment_rate", "multiple_alignment_rate", "num_aligned_any" ,"alignment_rate", "exec_time"]
 
 path = "/home/glick/JULIA-Take-2/src/slurm-*.out"
 output_file = "/home/labs/binford/single_sample_indexes/summary.csv"
@@ -21,10 +23,12 @@ with open(output_file, "a") as fh:
     for file in all_files:
         print(file)
         with open(file) as fh2:
+            slurm_job_name = file.split("-")[1].split(".out")[0]
+            slurm_time = subprocess.check_output(f'sacct --format="Elapsed" -j {slurm_job_name}', shell=True)
+            print(slurm_time.split("\n"))
             data = fh2.readlines()
             # This is gonna be gross
             try:
-
                 row = {
                     "index_sample": data[0].split(" ")[0].split("_")[1].strip(),
                     "reads_sample": data[0].split(" ")[1].split("_")[1].strip(),
@@ -32,6 +36,7 @@ with open(output_file, "a") as fh:
                     "num_aligned_none": int(data[3].split("(")[0].strip()),
                     "num_aligned_once": int(data[4].split("(")[0].strip()),
                     "num_aligned_multiple": int(data[5].split("(")[0].strip())
+                    "exec_time": ""
                 }
 
                 row["single_alignment_rate"] =  row["num_aligned_once"] / int(row["num_reads"])
